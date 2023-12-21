bf74f59aa885cbc4681152de67ea6c1389c2e056
eliasmg12345 <randyofgod666@gmail.com>
2023-12-21 15:00:41 -0400

    first commit

---
+++ src/app/dashboard/main/page.tsx
@@ -0,0 +1,8 @@
+
+export default function MainPage() {
+    return (
+        <div className="flex flex-col text-9xl items-center justify-center w-full h-full">
+            Main
+        </div>
+    )
+}
