cc9df3100ed2eb327954b0c9e0934a94046bb236
eliasmg12345 <randyofgod666@gmail.com>
2023-12-27 16:06:46 -0400

    static-botfound

--- src/app/dashboard/pokemons/[name]/page.tsx
+++ src/app/dashboard/pokemons/[name]/page.tsx
@@ -1,25 +1,34 @@
-import { Pokemon } from "@/pokemons";
+import { Pokemon, PokemonsResponse } from "@/pokemons";
 import { Metadata } from "next";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
 
 interface Props {
-  params: { id: string }
+  params: { name: string }
 }
 
 //solo ejecuta en builtime
 export async function generateStaticParams() {
-  const static151Pokemons = Array.from({ length: 151 }).map((v, i) => `${i + 1}`)
-  return static151Pokemons.map(id => ({
-    id: id
+
+  const data: PokemonsResponse = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=151`)
+    .then(res => res.json())
+
+  const static151Pokemons = data.results.map(pokemon => (
+    {
+      name: pokemon.name
+    }
+  ))
+
+  return static151Pokemons.map(({ name }) => ({
+    name
   }))
 }
 
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   try {
-    const { id, name } = await getPokemon(params.id)
+    const { id, name } = await getPokemon(params.name)
 
     return {
       title: `#${id} - ${name}`,
@@ -36,10 +45,10 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 
 }
 
-const getPokemon = async (id: string): Promise<Pokemon> => {
+const getPokemon = async (name: string): Promise<Pokemon> => {
   try {
 
-    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`, {
       //cache: 'force-cache',
       next: {
         revalidate: 60 * 60 * 30 * 6
@@ -56,7 +65,7 @@ const getPokemon = async (id: string): Promise<Pokemon> => {
 
 export default async function PokemonPage({ params }: Props) {
 
-  const pokemon = await getPokemon(params.id);
+  const pokemon = await getPokemon(params.name);
 
 
   return (
--- src/pokemons/components/PokemonCard.tsx
+++ src/pokemons/components/PokemonCard.tsx
@@ -12,7 +12,7 @@ export const PokemonCard = ({ pokemon }: Props) => {
     return (
 
         <div className="mx-auto right-0 mt-2 w-60">
-            <div className="bg-white rounded overflow-hidden shadow-lg">
+            <div className="flex flex-col bg-white rounded overflow-hidden shadow-lg">
                 <div className="flex flex-col items-center justify-center text-center p-6 bg-gray-800 border-b">
                     <Image
                         key={pokemon.id}
@@ -25,10 +25,10 @@ export const PokemonCard = ({ pokemon }: Props) => {
                     <p className="pt-2 text-lg font-semibold text-gray-50 capitalize">{name}</p>
                     <div className="mt-5">
                         <Link
-                            href={`/pokemon/${id}`}
+                            href={`/dashboard/pokemons/${name}`}
                             className="border rounded-full py-2 px-4 text-xs font-semibold text-gray-100"
                         >
-                            Mas Info
+                            Mas Información
                         </Link>
                     </div>
                 </div>
@@ -40,7 +40,7 @@ export const PokemonCard = ({ pokemon }: Props) => {
                         </div>
                         <div className="pl-3">
                             <p className="text-sm font-medium text-gray-800 leading-none">
-                                no favorito
+                                No favorito
                             </p>
                             <p className="text-xs text-gray-500">View your campaigns</p>
                         </div>
